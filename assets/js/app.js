48e907cafe508b231794b48dd35f796a7b9beebc
Ivaylo <MCflexa9999@gmail.com>
2026-03-05 10:13:50 +0100

    change app

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,30 +2,29 @@ import { GameModel } from "./Model/GameModel.js";
 import { GameView } from "./View/GameView.js";
 import { GameController } from "./Controller/GameController.js";
 import { selectData } from "./store/selectData.js";
-import { FormModel } from "./Model/FormModel.js"
+import { FormModel } from "./Model/FormModel.js";
 import { FormController } from "./Controller/FormController.js";
 import { FormView } from "./View/FormView.js";
 
-
-
-
-
 class App {
+  constructor() {
+    const file = window.location.pathname.split("/").pop() || "index.html";
 
-    constructor() {
-        const url = window.location.href;
-        const page = url.match(/[a-z]+.html/)[0];
-        switch (page) {
+    switch (file) {
+      case "index.html":
+        new GameController(new GameModel(selectData), new GameView());
+        break;
 
-            case "index.html":
-                new GameController(new GameModel(selectData), new GameView());
-                break;
-            case "form.html":
-                new FormController(new FormModel(), new FormView)
-                break;
-        }
+      case "form.html":
+        new FormController(new FormModel(), new FormView()); // also fix: missing ()
+        break;
 
+      default:
+        // optional fallback
+        new GameController(new GameModel(selectData), new GameView());
+        break;
     }
-
+  }
 }
-const app = new App();
+
+new App();
